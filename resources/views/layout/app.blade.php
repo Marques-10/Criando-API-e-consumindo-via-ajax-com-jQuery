2c8bbf7e6ea8c1d4e434c464e97080d83a2e10b9
Marques Lucas <xjmarques@hotmail.com>
2020-02-19 12:05:44 -0300

    Up Project

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>Cadastro de Produtos</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <style>
+            body {
+                padding: 20px;
+            }
+            .navbar {
+                margin-bottom: 20px;
+            }
+        </style>
+    </head>
+<body>
+    <div class="container">
+        @component('componente_navbar', [ "current" => $current ])
+        @endcomponent
+        <main role="main">
+            @hasSection('body')
+                @yield('body')
+            @endif
+        </main>
+    </div>
+    
+    <script src="{{ asset('js/app.js')}}" type="text/javascript"></script>
+    {{-- <script type="text/javascript">
+
+        let modal = document.querySelector('.novoProduto');
+        modal.addEventListener('click', (e) => {
+            $('#dlgProdutos').modal('show');
+        });
+
+    </script> --}}
+    @hasSection('javascript')
+        @yield('javascript')
+    @endif
+</body>
+</html>
